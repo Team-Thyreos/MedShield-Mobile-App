2c40af3304052f37d2ab8622c0b823b51ea85bfe
Michaelos10 <sundaymichael624@gmail.com>
2025-02-27 09:58:42 +0100

    minor update
    minor update

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,13 @@ import 'package:medshield/screens/input_page.dart';
 import 'package:device_preview/device_preview.dart'; // Helper method for reusable text boxes
 import 'package:firebase_core/firebase_core.dart';
 
-// void main() => runApp(
-//       DevicePreview(
-//         enabled: true, // Set to true to enable DevicePreview
-//         builder: (context) => BMICalculator(), // Your app widget
-//       ),
-//     );
-// void main() => runApp(BMICalculator());
-
 void main() async {
   // WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp(); // Connects to Firebase
-  runApp(BMICalculator());
+  runApp(HealthMonitor());
 }
 
-class BMICalculator extends StatelessWidget {
+class HealthMonitor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
